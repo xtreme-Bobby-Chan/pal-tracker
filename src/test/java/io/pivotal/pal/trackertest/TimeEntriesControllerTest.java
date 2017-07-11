80f1c5770a297b6342a6f0581aa3f448fc64b30c
Pivotal - Dev Floater 70 <pivotal@Dev-Floater-70.local>
2017-07-11 16:31:46 -0400

    Added Spring Actuator

--- src/test/java/io/pivotal/pal/trackertest/TimeEntriesControllerTest.java
+++ src/test/java/io/pivotal/pal/trackertest/TimeEntriesControllerTest.java
@@ -5,6 +5,8 @@ import io.pivotal.pal.tracker.TimeEntry;
 import io.pivotal.pal.tracker.TimeEntryRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
@@ -20,11 +22,15 @@ import static org.mockito.Mockito.verify;
 public class TimeEntriesControllerTest {
     TimeEntryRepository timeEntryRepository;
     TimeEntriesController controller;
+    CounterService counterService;
+    GaugeService gaugeService;
 
     @Before
     public void setUp() throws Exception {
         timeEntryRepository = mock(TimeEntryRepository.class);
-        controller = new TimeEntriesController(timeEntryRepository);
+        counterService = mock(CounterService.class);
+        gaugeService = mock(GaugeService.class);
+        controller = new TimeEntriesController(timeEntryRepository, counterService, gaugeService);
     }
 
     @Test
